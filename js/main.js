3d7dee0f06a60d74eae14ed8415f08e369015d94
Tiago Caetano <tiagoarcs@gmail.com>
2014-10-16 10:39:59 +0100

    animate

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
 speed=parseInt($('#txtspeed').val());
 console.log(speed);
+	
 	$('#bt2').click(function(){
 		speed=parseInt($('#txtspeed').val());
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.left +speed;
-		if (newpleft > 400) {
-			newpleft = 400;
+		if (newpleft < 500) {
+		$b.animate({ "left": "+=" + speed + "px"},"slow");	
+		//$b.css('left', newpleft + 'px');
 		}
-		$b.css('left', newpleft + 'px');
-	})
+		})
 
 	$('#bt1').click(function(){
 		speed=parseInt($('#txtspeed').val());
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.left -speed;
-		if (newpleft < 40) {
-			newpleft = 40;
-		}
-		$b.css('left', newpleft + 'px');
+		if (newpleft > 40) {
+		$b.animate({ "left": "-=" + speed + "px"},"slow");
+		//$b.css('left', newpleft + 'px');
+	}
 	})
 
 	$('#bt3').click(function(){
@@ -28,9 +29,9 @@ console.log(speed);
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.top +speed;
-		$b.css('top', newpleft + 'px');
-		if (newpleft > 300) {
-			newpleft = 300;
+		//$b.css('top', newpleft + 'px');
+		if (newpleft < 150) {
+		$b.animate({ "top": "+=" + speed + "px"},"slow");
 		}
 	})
 
@@ -39,9 +40,9 @@ console.log(speed);
 		$b = $( "#boneco" );
 		position = $b.position();
 		newpleft = position.top -speed;
-		$b.css('top', newpleft + 'px');
-		if (newpleft < 0) {
-			newpleft = 0;
-		}
+		//$b.css('top', newpleft + 'px');
+		if (newpleft > 150) {
+		$b.animate({ "top": "-=" + speed + "px"},"slow");
+	}
 	})
 });
